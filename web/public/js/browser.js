9ea6d8dbe4f170d76c4dfd57b943e1176164b1c5
CzlowiekImadlo <quak@tlen.pl>
2016-11-18 20:11:40 +0100

    Added map box rendering

--- web/public/js/browser.js
+++ web/public/js/browser.js
@@ -3,6 +3,7 @@ var browser = {
     api_location : null,
     load_button : null,
     load_spinner : null,
+    box_container : null,
 
     last_loaded_id : null,
 
@@ -10,6 +11,7 @@ var browser = {
         this.api_location = configuration.api_location;
         this.load_button = $("#load_more button");
         this.load_spinner = $("#spinner");
+        this.box_container = $("#listing");
 
         this.load_spinner.hide()
     },
@@ -25,10 +27,16 @@ var browser = {
     },
 
     appendLoadedData : function(data) {
-        console.log(data);
+        var arrayLength = data['maps'].length;
+        for (var i = 0; i < arrayLength; i++) {
+            if (browser.last_loaded_id === null || browser.last_loaded_id > data['maps'][i]['id']) {
+                browser.last_loaded_id = data['maps'][i]['id'];
+            }
+            browser.box_container.appendChild($("<div class='map'><h3>" + data['maps'][i]['name'] + "</h3><span class='created'>" + data['maps'][i]['created'] + "</span><span class='code'>" + data['maps'][i]['code'] + "</span></div>"));
+        }
         browser.load_spinner.hide()
         if (browser.last_loaded_id > 0) {
-            browser.load_button.show()
+            browser.load_button.show();
         }
     }
 }
